a9784b513e434e3775899775d0e1c714ff7df23a
Joshua <joshuaystang@gmail.com>
2019-05-01 16:19:00 +1000

    tidied up get experience functions

--- linked_rp.py
+++ linked_rp.py
@@ -55,9 +55,11 @@ def get_summary(driver):
 
 
 def get_experience(driver, exp_section):
+    # Load experience section
     driver.execute_script("arguments[0].scrollIntoView(true);", exp_section)
     time.sleep(1)
 
+    # Locate individual experiences
     exp_ul = exp_section.find_element(
         By.CSS_SELECTOR, '.pv-profile-section__section-info.section-info.pv-profile-section__section-info--has-no-more')
     exp_divs = exp_ul.find_elements(
@@ -68,93 +70,92 @@ def get_experience(driver, exp_section):
         try:
             exp_summary = exp_div.find_element(
                 By.CSS_SELECTOR, '.pv-entity__summary-info.pv-entity__summary-info--background-section')
-            title = exp_summary.find_element(By.CSS_SELECTOR, '.t-16.t-black.t-bold').text
-            company = exp_summary.find_element_by_class_name('pv-entity__secondary-title').text
-            dates_section = exp_summary.find_element(
-                By.CSS_SELECTOR, '.pv-entity__date-range.t-14.t-black--light.t-normal')
-            dates = dates_section.find_elements_by_tag_name('span')[1].text
-
-            try:
-                location_section = exp_summary.find_element(
-                    By.CSS_SELECTOR, '.pv-entity__location.t-14.t-black--light.t-normal.block')
-                location = location_section.find_elements_by_tag_name('span')[1].text
-            except NoSuchElementException:
-                location = ''
-
-            try:
-                description_section = exp_div.find_element(
-                    By.CSS_SELECTOR, '.pv-entity__description.t-14.t-black.t-normal.ember-view')
-                more_btn_section = description_section.find_element_by_class_name('lt-line-clamp__ellipsis')
-
-                if 'lt-line-clamp__ellipsis--dummy' in more_btn_section.get_attribute('class'):
-                    description = description_section.text
-                else:
-                    more_btn_section.find_element_by_class_name('lt-line-clamp__more').click()
-                    description = description_section.find_element_by_class_name('lt-line-clamp__raw-line').text
-
-            except NoSuchElementException:
-                description = ''
-
-            exps.append({
-                'title': title,
-                'company': company,
-                'dates': dates,
-                'location': location,
-                'description': description
-            })
+            exps.append(get_single_role(exp_div, exp_summary))
         except NoSuchElementException:
             exp_summary = exp_div.find_element_by_class_name('pv-entity__company-summary-info-v2')
-            company_section = exp_summary.find_element_by_tag_name('h3')
-            company = company_section.find_elements_by_tag_name('span')[1].text
-
-            # Show all roles
-            try:
-                driver.find_element_by_class_name('pv-profile-section__toggle-detail-icon').click()
-                time.sleep(1)
-            except NoSuchElementException:
-                pass
-
-            roles = []
-            for role_section in exp_div.find_elements_by_class_name('pv-entity__position-group-role-item'):
-                title_section = role_section.find_element(By.CSS_SELECTOR, '.t-14.t-black.t-bold')
-                title = title_section.find_elements_by_tag_name('span')[1].text
-                dates_section = role_section.find_element(
-                    By.CSS_SELECTOR, '.pv-entity__date-range.t-14.t-black.t-normal')
-                dates = dates_section.find_elements_by_tag_name('span')[1].text
-
-                try:
-                    location_section = role_section.find_element(
-                        By.CSS_SELECTOR, '.pv-entity__location.t-14.t-black--light.t-normal.block')
-                    location = location_section.find_elements_by_tag_name('span')[1].text
-                except NoSuchElementException:
-                    location = ''
-
-                try:
-                    description_section = role_section.find_element(
-                        By.CSS_SELECTOR, '.pv-entity__description.t-14.t-black.t-normal.ember-view')
-                    more_btn_section = description_section.find_element_by_class_name('lt-line-clamp__ellipsis')
-
-                    if 'lt-line-clamp__ellipsis--dummy' in more_btn_section.get_attribute('class'):
-                        description = description_section.text
-                    else:
-                        more_btn_section.find_element_by_class_name('lt-line-clamp__more').click()
-                        description = description_section.find_element_by_class_name('lt-line-clamp__raw-line').text
-                except NoSuchElementException:
-                    description = ''
-
-                roles.append({
-                    'title': title,
-                    'dates': dates,
-                    'location': location,
-                    'description': description
-                })
-
-            exps.append({
-                'company': company,
-                'roles': roles
-            })
+            exps.append(get_multiple_roles(exp_div, exp_summary))
 
     return exps
+
+
+def get_single_role(exp_div, exp_summary):
+    title = exp_summary.find_element(By.CSS_SELECTOR, '.t-16.t-black.t-bold').text
+    company = exp_summary.find_element_by_class_name('pv-entity__secondary-title').text
+    dates = get_span_text(exp_summary, '.pv-entity__date-range.t-14.t-black--light.t-normal')
+    location = get_experience_location(exp_summary)
+    description = get_experience_description(exp_div)
+
+    results = {
+        'company': company,
+        'roles': [{
+            'title': title,
+            'dates': dates,
+            'location': location,
+            'description': description
+        }]
+    }
+
+    return results
+
+
+def get_multiple_roles(exp_div, exp_summary):
+    company = get_span_text(exp_summary, '.t-16.t-black.t-bold')
+
+    # Show all roles
+    try:
+        exp_div.find_element_by_class_name('pv-profile-section__toggle-detail-icon').click()
+        time.sleep(1)
+    except NoSuchElementException:
+        pass
+
+    roles = []
+    for role_section in exp_div.find_elements_by_class_name('pv-entity__position-group-role-item'):
+        title = get_span_text(role_section, '.t-14.t-black.t-bold')
+        dates = get_span_text(role_section, '.pv-entity__date-range.t-14.t-black.t-normal')
+        location = get_experience_location(role_section)
+        description = get_experience_description(role_section)
+
+        roles.append({
+            'title': title,
+            'dates': dates,
+            'location': location,
+            'description': description
+        })
+
+    results = {
+        'company': company,
+        'roles': roles
+    }
+
+    return results
+
+
+def get_span_text(element, name):
+    return element.find_element(By.CSS_SELECTOR, name).find_elements_by_tag_name('span')[1].text.replace('–', '-')
+
+
+def get_experience_location(element):
+    try:
+        return get_span_text(element, '.pv-entity__location.t-14.t-black--light.t-normal.block')
+    except NoSuchElementException:
+        return ''
+
+
+def get_experience_description(element):
+    try:
+        description_section = element.find_element(
+            By.CSS_SELECTOR, '.pv-entity__description.t-14.t-black.t-normal.ember-view')
+        more_btn_section = description_section.find_element_by_class_name('lt-line-clamp__ellipsis')
+
+        if 'lt-line-clamp__ellipsis--dummy' in more_btn_section.get_attribute('class'):
+            description = description_section.text
+        else:
+            more_btn_section.find_element_by_class_name('lt-line-clamp__more').click()
+            description = description_section.find_element_by_class_name('lt-line-clamp__raw-line').text
+    except NoSuchElementException:
+        description = ''
+
+    return description
 
 
 if __name__ == '__main__':
