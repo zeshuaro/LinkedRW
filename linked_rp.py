058262a8de9b746ee04fac52964f8d25bc7217c0
Joshua <joshuaystang@gmail.com>
2019-05-02 16:03:38 +1000

    added to get name and contact

--- linked_rp.py
+++ linked_rp.py
@@ -5,6 +5,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
+from urllib.parse import urlparse
 
 from accomplishment import get_accomplishment_details
 from background import get_background_details
@@ -35,6 +36,8 @@ def main():
     WebDriverWait(driver, TIMEOUT).until(ec.presence_of_element_located((By.ID, 'oc-background-section')))
 
     profile = {
+        'name': driver.find_element_by_css_selector('.pv-top-card-section__name.inline.t-24.t-black.t-normal').text,
+        'contact': get_contact(driver),
         'summary': get_summary(driver),
         'experience': get_background_details(driver, By.ID, 'experience-section', EXPERIENCE),
         'education': get_background_details(driver, By.ID, 'education-section', EDUCATION),
@@ -70,6 +73,47 @@ def get_summary(driver):
         pass
 
     return driver.find_element_by_css_selector('.pv-top-card-section__summary-text.text-align-left.mt4.ember-view').text
+
+
+def get_contact(driver):
+    driver.find_element_by_xpath("//a[@data-control-name='contact_see_more']").click()
+
+    email_section = WebDriverWait(driver, TIMEOUT).until(ec.presence_of_element_located((
+        By.CSS_SELECTOR, '.pv-contact-info__contact-type.ci-email')))
+    email = email_section.find_element_by_tag_name('a').text
+
+    try:
+        phone_section = driver.find_element_by_css_selector('.pv-contact-info__contact-type.ci-phone')
+        phone = phone_section.find_element_by_css_selector('.t-14.t-black.t-normal').text
+    except NoSuchElementException:
+        phone = ''
+
+    try:
+        address_section = driver.find_element_by_css_selector('.pv-contact-info__contact-type.ci-address')
+        address = address_section.find_element_by_tag_name('a').text
+    except NoSuchElementException:
+        address = ''
+
+    github_id = scholar_id = ''
+    websites_section = driver.find_element_by_css_selector('.pv-contact-info__contact-type.ci-websites')
+
+    for li in websites_section.find_elements_by_tag_name('li'):
+        link = li.find_element_by_tag_name('a').get_attribute('href')
+        if 'github' in link:
+            github_id = urlparse(link).path.strip('/')
+        elif 'scholar' in link:
+            scholar_id = urlparse(link).query.lstrip('user=')
+
+    driver.find_element_by_class_name('artdeco-dismiss').click()
+    results = {
+        'phone': phone,
+        'address': address,
+        'email': email,
+        'github_id': github_id,
+        'scholar_id': scholar_id
+    }
+
+    return results
 
 
 if __name__ == '__main__':
